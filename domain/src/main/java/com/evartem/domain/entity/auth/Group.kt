5c608e471c14846a7fa36091af24195cadc2d95f
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-01 15:47:06 +0500

    Add comments

--- domain/src/main/java/com/evartem/domain/entity/auth/Group.kt
+++ domain/src/main/java/com/evartem/domain/entity/auth/Group.kt
@@ -1,5 +1,13 @@
 package com.evartem.domain.entity.auth
 
+/**
+ * A group represents a few permissions combined together. A user that is a member of a group, has all the
+ * permissions that the group has. A user can be a member of a few groups.
+ *
+ * @property id the group's id on the back-end
+ * @property description a name/description of the group
+ * @property permissions a list of permissions that the group owns
+ */
 data class Group(
     val id: Int,
     val description: String,
--- domain/src/main/java/com/evartem/domain/entity/auth/User.kt
+++ domain/src/main/java/com/evartem/domain/entity/auth/User.kt
@@ -6,7 +6,7 @@ package com.evartem.domain.entity.auth
  * @property id the user's id on the back-end
  * @property fullName the user's full name
  * @property position the user's position in the company (e.g. Senior accountant)
- * @property authGroups the authorization groups that the user belongs to
+ * @property authGroups the authorization groups that the user is a member of
  * @property status the state of the user (busy, off-duty, waiting for a task, etc.)
  */
 data class User(
