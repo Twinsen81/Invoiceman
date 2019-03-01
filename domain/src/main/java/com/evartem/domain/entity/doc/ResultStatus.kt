e00b616ad14ed530266c6433b0972e6f047a0771
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-01 15:48:02 +0500

    Remove unnecessary status, add comments

--- domain/src/main/java/com/evartem/domain/entity/doc/ResultStatus.kt
+++ domain/src/main/java/com/evartem/domain/entity/doc/ResultStatus.kt
@@ -1,7 +1,11 @@
 package com.evartem.domain.entity.doc
 
+/**
+ * The status of processing a packaging by an employee - represents the state of
+ * the operation. If the employee successfully scanned the serial number, then
+ * the operation is completed, else - failed.
+ */
 enum class ResultStatus {
-    COMPLETED,
-    IN_PROGRESS,
-    ERROR
+    /** the processing succeeded - got the result*/ COMPLETED,
+    /** the processing failed - no result*/ FAILED
 }
