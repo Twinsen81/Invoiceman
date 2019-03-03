ebb90e255c2f6d08eee6bee3039b5c3d7546ff92
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-04 02:39:54 +0500

    Add test code for the first run

--- app/src/main/java/com/evartem/invoiceman/MainActivity.kt
+++ app/src/main/java/com/evartem/invoiceman/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.evartem.invoiceman
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import com.evartem.domain.entity.doc.Invoice
+import com.evartem.domain.entity.doc.Product
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,10 +28,21 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    val inv = Invoice(
+        UUID.randomUUID().toString(), 120, "23.09.19", "Siemens",
+        listOf(
+            Product(1, "6ES7-RTA-OAA0", "PLC", 2),
+            Product(2, "6ES7-BBNF-OAA0", "Module", 1)
+        ))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        message.setOnClickListener {
+            message.text = inv.products[1].article
+        }
     }
 }
