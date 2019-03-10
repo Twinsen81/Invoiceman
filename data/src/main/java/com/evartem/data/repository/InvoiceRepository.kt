467ab061fdefd03d4f69dcb6d8fa8d8c107ecbfc
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-11 02:24:25 +0500

    Comment out unimplemented methods

--- data/src/main/java/com/evartem/data/repository/InvoiceRepository.kt
+++ data/src/main/java/com/evartem/data/repository/InvoiceRepository.kt
@@ -4,8 +4,6 @@ import com.evartem.data.local.InvoiceLocalDataSource
 import com.evartem.data.local.model.InvoiceLocalModel
 import com.evartem.data.local.model.ResultLocalModel
 import com.evartem.data.remote.api.InvoiceService
-import com.evartem.data.remote.model.InvoiceRemoteModel
-import io.reactivex.Observable
 
 class InvoiceRepository(private val localDataSource: InvoiceLocalDataSource,
                         private val remoteDataSource: InvoiceService) {
@@ -21,7 +19,7 @@ class InvoiceRepository(private val localDataSource: InvoiceLocalDataSource,
 
     }
 
-    fun getInvoicesForUser(userId: String): Observable<List<InvoiceRemoteModel>> {
+    /*fun getInvoicesForUser(userId: String): Observable<List<InvoiceRemoteModel>> {
 
-    }
+    }*/
 }
