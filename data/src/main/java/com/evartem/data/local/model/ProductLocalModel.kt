f3b9ff4d4a93220890a32dcae14b38b7fec5f6f9
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-12 00:56:40 +0500

    Clean up code

--- data/src/main/java/com/evartem/data/local/model/ProductLocalModel.kt
+++ data/src/main/java/com/evartem/data/local/model/ProductLocalModel.kt
@@ -2,7 +2,6 @@ package com.evartem.data.local.model
 
 import io.realm.RealmList
 import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
 
 open class ProductLocalModel(
     var id: Int = 0,
