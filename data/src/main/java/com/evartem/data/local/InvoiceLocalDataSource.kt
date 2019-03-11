e97141a8d97e2228b8dfd36c0fdf68e0b6ab1246
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-11 09:52:37 +0500

    Remove temporary code

--- data/src/main/java/com/evartem/data/local/InvoiceLocalDataSource.kt
+++ data/src/main/java/com/evartem/data/local/InvoiceLocalDataSource.kt
@@ -24,8 +24,6 @@ class InvoiceLocalDataSource {
                 it.insertOrUpdate(invoices)
             }
         }
-
-        RealmConfiguration.Builder().inMemory().name("test-realm").build()
     }
 
     fun insertOrUpdateInvoice(invoice: InvoiceLocalModel) {
