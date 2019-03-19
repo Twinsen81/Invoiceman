3a52aab9f9c5c6afddbde86f5dff3d68f34b6d69
Evgeniy Plokhov <plokhov@gmail.com>
2019-03-20 00:16:37 +0500

    Add LeakCanary

--- app/src/main/java/com/evartem/invoiceman/TheApp.kt
+++ app/src/main/java/com/evartem/invoiceman/TheApp.kt
@@ -1,12 +1,20 @@
 package com.evartem.invoiceman
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
-class TheApp: Application() {
+class TheApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
     }
 
     companion object {
